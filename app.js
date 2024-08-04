5d7c7d9021244947d38ee10564aa12d6c9490352
dalancoder <mustafadalan0@gmail.com>
2024-08-04 22:51:12 +0300

    yeni düzenleme

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-/* ------------------------------------------------------ */
-/*                        Variables                       */
-/* ------------------------------------------------------ */
-const cards = document.querySelector(".cards");
-const deleteAll = document.querySelector("deleteAll");
-const inputText = document.querySelector("inputText");
-const select = document.querySelector("select");
-const btn = document.querySelector("btn");
-
-
-    if(inputText.value == '' ){
-        btn.disabled=true;
+function addItem() {
+    let inputValue = document.getElementById("inputValue").value.trim();
+    const selectedDivId = document.querySelector("#selectBox").value;
+    //Burda önemli olan select.value su ile div idleri aynı veriyor olmamız
+    const selectedDiv = document.getElementById(selectedDivId);
+        // Yeni bir liste öğesi oluşturma
+    const newItem = document.createElement('li');
+        //   newItem.textContent = inputValue;
+          newItem.className = 'list-item';
+          newItem.innerHTML = `
+              ${inputValue}
+              <button onclick="deleteItem(this)"><i class="fas fa-trash"></i></button>
+              <button onclick="toggleDone(this)"><i class="fas fa-check"></i></button>
+           `;
+        if(!inputValue) return;
+        // Liste öğesini div'e ekleme
+    const ol = selectedDiv.querySelector('ol') || document.createElement('ol');
+        ol.appendChild(newItem);
+        selectedDiv.appendChild(ol);
+        document.getElementById("inputValue").value= '';
+        // Input'u temizleme
     }
-    
-deleteAll.style.backgroundColor ="red";
-
-inputText.focus();
+    function toggleDone(button) {
+        // Butona tıklanan liste öğesinin stilini değiştir
+        const listItem = button.parentElement;
+        listItem.classList.toggle('esnek');
+    }
+    function deleteItem(button) {
+        // Butona tıklanan liste öğesini sil
+        const listItem = button.parentElement;
+        listItem.remove();
+    }
+    function removeItemAll(x) {
+        const divDelete = document.getElementById(x);
+        const delOl = divDelete.querySelector("ol");
+        delOl.innerHTML="";
+    }
